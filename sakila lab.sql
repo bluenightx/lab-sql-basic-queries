c1e3e3fecfb8416984a5f4f7d0135bf031d53d65
ana <anaisasena91@hotmail.com>
2025-06-09 22:54:27 +0100

    Solved Lab

---
+++ sakila lab.sql
@@ -0,0 +1,60 @@
+USE sakila; #Select the database
+
+#1 Display all available tables in the Sakila database.
+SHOW TABLES;
+
+#2 Retrieve all the data from the tables actor, film and customer.
+SELECT *FROM actor, film, customer;
+
+#3.1 Titles of all films from the film table
+SELECT title
+FROM film;
+
+#3.2 List of languages used in films, with the column aliased as language from the language table
+SELECT name AS language
+FROM language;
+
+#3.3 List of first names of all employees from the staff table
+SELECT first_name
+FROM staff;
+
+#4 Retrieve unique release years
+SELECT DISTINCT release_year
+FROM film;
+
+#5.1 Determine the number of stores that the company has.
+SELECT COUNT(store_id)
+FROM store;
+
+#5.2 Determine the number of employees that the company has.
+SELECT COUNT(staff_id)
+FROM staff;
+
+#5.3 Determine how many films are available for rent and how many have been rented.
+SELECT COUNT(inventory_id) AS films_available
+FROM inventory 
+WHERE inventory_id NOT IN (
+SELECT inventory_id
+FROM rental
+WHERE return_date IS NULL );
+
+
+SELECT COUNT(rentaL_id) AS total_rentals
+FROM rental;
+
+
+#5.4 Determine the number of distinct last names of the actors in the database.
+SELECT DISTINCT COUNT(last_name)
+FROM actor;
+
+#6 Retrieve the 10 longest films.
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+
+#7.1 Retrieve all actors with the first name "SCARLETT".
+SELECT first_name
+FROM actor
+WHERE first_name = "SCARLETT" ;
